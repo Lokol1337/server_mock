091812a932b73ebc777079b8d71e737c4df2cbc8
lokol <drejfeld@gmail.com>
2022-10-29 12:35:53 +0300

    ws: ответ

--- server.js
+++ server.js
@@ -42,25 +42,24 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+var array = new Map;
 
-app.use(function (req, res, next) {
-  console.log('middleware');
-  req.testing = 'testing';
-  return next();
-});
+// app.use(function (req, res, next) {
+//   req.testing = 'testing';
+//   return next();
+// });
 
-app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
-  res.end();
-});
+// app.get('/', function(req, res, next){
+//   console.log('get route', req.testing);
+//   res.end();
+// });
 
 app.ws('/', function(ws, req) {
   ws.on('message', function(msg) {
     map = new Map(JSON.parse(msg));
     console.log(map);
-    ws.send('hello');
+    ws.send(JSON.stringify(Array.from(map.entries())));
   });
-  console.log('socket', req.testing);
 
 });
 
